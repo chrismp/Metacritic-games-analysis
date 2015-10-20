5b7670922b3ae3861366490d634257a3543ee432
unknown <>
2015-10-19 22:24:59 -0400

    added more single-variable analyses

--- script.R
+++ script.R
@@ -32,19 +32,143 @@ games$ReleaseQuarter <- ifelse(games$ReleaseMonth <= 3, 1,
                                                         4))) # Add quarter column
 games <- mutate(games, ReleaseYearQuarter = paste(ReleaseYear,ReleaseQuarter,sep='Q')) # Combine year and quarter columns into new column
 
+summary(games) # Min, Max, Median and other summary stats for each variable/column
 
+# System count
 ggplot(
   count(games,System),
   aes(
     x = reorder(System, n), 
     y = n
   )
-) + labs(x=NULL, y="Count") + 
+) + ggtitle("Systems") +
+  labs(x=NULL, y="Count") + 
   geom_bar(stat="identity") + 
   coord_flip() + 
   scale_y_continuous(breaks=seq(0,4000,500))
 
-summary(games) # Min, Max, Median and other summary stats for each variable/column
+# Publisher count
+ggplot(
+  top_n(
+    count(games,Publisher),
+    20, # Show top 20
+    n # Order by `n` variable, aka the count
+  ),
+  aes(
+    x = reorder(Publisher, n), 
+    y = n
+  )
+) + ggtitle("Publishers") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+# Developer count
+ggplot(
+  top_n(
+    count(games,Developer),
+    20, # Show top 20
+    n # Order by `n` variable, aka the count
+  ),
+  aes(
+    x = reorder(Developer, n), 
+    y = n
+  )
+) + ggtitle("Developers") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+# Genre count
+ggplot(
+  top_n(
+    count(games,Genre),
+    20, # Show top 20
+    n # Order by `n` variable, aka the count
+  ),
+  aes(
+    x = reorder(Genre, n), 
+    y = n
+  )
+) + ggtitle("Genres") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+# ESRB count
+ggplot(
+  top_n(
+    count(games,ESRB),
+    20, # Show top 20
+    n # Order by `n` variable, aka the count
+  ),
+  aes(
+    x = reorder(ESRB, n), 
+    y = n
+  )
+) + ggtitle("ESRB ratings") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+
+# Metascore categories count
+ggplot(
+  count(games,MetascoreCategory),
+  aes(
+    x = reorder(MetascoreCategory, n), 
+    y = n
+  )
+) + ggtitle("Metascore categories") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+# User score categories count
+ggplot(
+  count(games,UserScoreCategory),
+  aes(
+    x = reorder(UserScoreCategory, n), 
+    y = n
+  )
+) + ggtitle("User score categories") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+# Release years
+ggplot(
+  count(games,ReleaseYear),
+  aes(
+    x = factor(
+      ReleaseYear, 
+      levels=rev(
+        levels(factor(ReleaseYear))
+      )
+    ),
+    y = n
+  )
+) + ggtitle("Release years") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
+
+# Release months
+ggplot(
+  count(games,ReleaseMonth),
+  aes(
+    x = factor(
+      ReleaseMonth, 
+      levels=rev(
+        levels(factor(ReleaseMonth))
+      )
+    ),
+    y = n
+  )
+) + ggtitle("Release months") +
+  labs(x=NULL, y="Count") + 
+  geom_bar(stat="identity") + 
+  coord_flip() 
 
 # Histogram, Metascore distribution
 ggplot(games, aes(x=Metascore)) + 
@@ -54,6 +178,7 @@ ggplot(games, aes(x=Metascore)) +
     color="red"
   )
 
+
 # Histogram, User score distribution
 ggplot(games, aes(x=UserScore)) + 
   geom_histogram(binwidth=0.1) +
@@ -61,9 +186,6 @@ ggplot(games, aes(x=UserScore)) +
     aes(xintercept=mean(UserScore)),
     color="red"
   )
-
-
-
 
 
 # Boxplot, Metascore by system
